0b20fd363493cc20c7d256c734e74f94d025d573
neppiness <0414kjh@naver.com>
2022-06-02 22:44:45 +0900

    Commit on June 1-2, 2022

--- Computer science/Algorithm/all-combinations-05-31-2022.py
+++ Computer science/Algorithm/all-combinations-05-31-2022.py
@@ -1,7 +1,29 @@
-answers = []
+answers = []; answer = []
 
-def all_combinations(total_no_of_elements, no_of_elements_to_be_picked):
+total_no_of_elements = 2
+no_of_elements_to_be_picked = 1
+current_index = 0;
+
+def all_combinations(total_no_of_elements, no_of_elements_to_be_picked, current_index):
+    # Base case
     if (no_of_elements_to_be_picked == 0):
-        return 0
+        answers.append(answer)
+        answer.pop()
+        return all_combinations(total_no_of_elements, no_of_elements_to_be_picked + 1, current_index)
+
+    answer.append(current_index)
     no_of_elements_to_be_picked -= 1
-    return all_combinations(total_no_of_elements, no_of_elements_to_be_picked)
+
+    current_index += 1
+
+    if current_index != total_no_of_elements - 1:
+        return all_combinations(total_no_of_elements, no_of_elements_to_be_picked, current_index)
+    else:
+        answer.pop()
+        
+        print(answer[-1])
+        current_index = answer[-1]
+        return all_combinations(total_no_of_elements, no_of_elements_to_be_picked + 1, current_index)
+
+answers= all_combinations(total_no_of_elements, no_of_elements_to_be_picked, current_index)
+print(answers)

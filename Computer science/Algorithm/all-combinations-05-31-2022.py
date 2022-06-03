9e716f46d07eca30ef413c029e479f2d4f78886c
neppiness <0414kjh@naver.com>
2022-06-03 16:43:36 +0900

    Build a brute-force algorithm to find the all combinations using recursive function written in Python

--- Computer science/Algorithm/all-combinations-05-31-2022.py
+++ Computer science/Algorithm/all-combinations-05-31-2022.py
@@ -1,29 +1,38 @@
-answers = []; answer = []
-
-total_no_of_elements = 2
-no_of_elements_to_be_picked = 1
-current_index = 0;
-
-def all_combinations(total_no_of_elements, no_of_elements_to_be_picked, current_index):
+def all_combinations(total_no_of_elements, no_of_elements_to_be_picked, initial_index):
     # Base case
     if (no_of_elements_to_be_picked == 0):
-        answers.append(answer)
-        answer.pop()
-        return all_combinations(total_no_of_elements, no_of_elements_to_be_picked + 1, current_index)
+        answers.append(answer[:])
+        return 0
 
-    answer.append(current_index)
     no_of_elements_to_be_picked -= 1
 
-    current_index += 1
-
-    if current_index != total_no_of_elements - 1:
-        return all_combinations(total_no_of_elements, no_of_elements_to_be_picked, current_index)
-    else:
+    for current_index in range(initial_index, total_no_of_elements):
+        answer.append(current_index)
+        next_index = find_smallest_value(answer)
+        all_combinations(total_no_of_elements, no_of_elements_to_be_picked, next_index)
         answer.pop()
-        
-        print(answer[-1])
-        current_index = answer[-1]
-        return all_combinations(total_no_of_elements, no_of_elements_to_be_picked + 1, current_index)
 
-answers= all_combinations(total_no_of_elements, no_of_elements_to_be_picked, current_index)
-print(answers)
+def find_smallest_value(answer):
+    if answer == []:
+        smallest = 0
+    else:
+        smallest = answer[-1] + 1
+    return smallest
+
+answers = []
+answer = []
+initial_index = 0
+
+print('Enter the total number of elements in integer')
+total_no_of_elements = input()
+total_no_of_elements = int(total_no_of_elements)
+
+print('Enter the number of elements to be picked integer')
+no_of_elements_to_be_picked = input()
+no_of_elements_to_be_picked = int(no_of_elements_to_be_picked)
+
+if total_no_of_elements >= no_of_elements_to_be_picked:
+    all_combinations(total_no_of_elements, no_of_elements_to_be_picked, initial_index)
+    print(answers)
+else:
+    print('An error occurred')

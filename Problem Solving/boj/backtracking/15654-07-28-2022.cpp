ffbe7e533430273b8ff2b320d29e0388cb63f9dc
neppiness <0414kjh@naver.com>
2022-07-28 22:23:36 +0900

    3rd commit on July 28th, 2022

--- Problem Solving/boj/backtracking/15654-07-28-2022.cpp
+++ Problem Solving/boj/backtracking/15654-07-28-2022.cpp
@@ -1,18 +1,34 @@
 #include <iostream>
+#include <algorithm>
 #include <vector>
 
 using namespace std;
 
-int N, M;
+int N, M, num[10], ans[10];
+bool isUsed[10] = {false};
+
+void search(int m, vector<int> &intV) {
+    if (M == m) {
+        for (int i = 0; i < M; i++) cout << ans[i] << ' ';
+        cout << '\n';
+    } else for(int i = 0; i < N; i++) {
+        if(isUsed[intV[i]] == true) continue;
+        ans[m] = intV[i];
+        isUsed[intV[i]] = true;
+        search(m+1, intV);
+        isUsed[intV[i]] = false;
+    }
+    
+}
+
 int main(){
     ios::sync_with_stdio(0); cin.tie(0);
 
     cin >> N >> M;
+    for(int i = 0; i < N; i++) cin >> num[i];
 
-    int input;
-    vector<int> intV;
+    vector<int> intV(num, num + N);
+    sort(intV.begin(), intV.end());
 
-    for(int i = 0; i < N; i++) {cin >> input; intV.insert(input, i);}
-
-
+    search(0, intV);
 }

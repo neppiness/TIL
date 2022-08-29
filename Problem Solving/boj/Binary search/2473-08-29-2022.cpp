5e585964796b988e29397274e79f3452f87a1166
neppiness <0414kjh@naver.com>
2022-08-29 20:43:01 +0900

    2nd Commit on August 29th, 2022

--- Problem Solving/boj/Binary search/2473-08-29-2022.cpp
+++ Problem Solving/boj/Binary search/2473-08-29-2022.cpp
@@ -6,8 +6,18 @@
 using namespace std;
 
 int N; 
-int A[5002];
-int a1, a2, a3, RES = 0x7ffffff;
+ll A[5002];
+int a1, a2, a3;
+ll RES = 0x7ffffff;
+
+int LB(int begin, int end, int val) {
+    while(begin < end) {
+        int mid = (begin + end)/2;
+        if(A[mid] > val) end = mid;
+        else begin = mid + 1;
+    }
+    return begin;
+}
 
 int main(){
     ios::sync_with_stdio(0); cin.tie(0);
@@ -20,20 +30,25 @@ int main(){
     for(int i = 0; i < N; i++)
         for(int j = i + 1; j < N; j++) {
             ll tar = -(A[i] + A[j]);
-            int idx = lower_bound(A+j+1, A+N, tar) - &A[0] - 1;
-            ll res = A[idx] - tar;
-            if(res < 0) res = -res;
-            if(res < RES) {
-                RES = res;
-                a1 = i; a2 = j; a3 = idx;
-            }
-            if(idx + 1 < N) {
-                idx++;
+            int idx = LB(j+1, N, tar) - 1;
+            
+            if(idx != i && idx != j) {
                 ll res = A[idx] - tar;
                 if(res < 0) res = -res;
                 if(res < RES) {
                     RES = res;
                     a1 = i; a2 = j; a3 = idx;
+                }
+            }
+            if(idx + 1 < N) {
+                idx++;
+                if(idx != i && idx != j) {
+                    ll res = A[idx] - tar;
+                    if(res < 0) res = -res;
+                    if(res < RES) {
+                        RES = res;
+                        a1 = i; a2 = j; a3 = idx;
+                    }
                 }
             }
         }

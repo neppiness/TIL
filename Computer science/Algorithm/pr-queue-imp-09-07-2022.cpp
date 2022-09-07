a602fc8c6b79b144b8f183eaae33400da1a01a4d
neppiness <0414kjh@naver.com>
2022-09-08 01:09:45 +0900

    1st commit on September 8th, 2022

---
+++ Computer science/Algorithm/pr-queue-imp-09-07-2022.cpp
@@ -0,0 +1,47 @@
+// based on https://github.com/encrypted-def/basic-algo-lecture/blob/master/0x17/heap_test.cpp
+#include <bits/stdc++.h>
+using namespace std;
+
+int heap[100005];
+int sz = 0; // heap에 들어있는 원소의 수
+
+void push(int x){
+  sz++; heap[sz] = x;
+  int cur = sz;
+  while(cur != 1) {
+    if(heap[cur/2] > heap[cur])
+      swap(heap[cur/2], heap[cur]);
+    cur /= 2;
+  }
+}
+
+int top(){return heap[1];}
+
+void pop(){
+  heap[1] = heap[sz];
+  sz--;
+  int cur = 1;
+  while(cur <= sz) {
+    int tmp;
+    if(heap[cur*2] > heap[cur*2 + 1]) tmp = cur*2;
+    else tmp = cur*2 + 1;
+    swap(heap[tmp], heap[cur]);
+    swap(tmp, cur);
+  }
+}
+
+void test(){
+  push(10); push(2); push(5); push(9); // {10, 2, 5, 9}
+  cout << top() << '\n'; // 2
+  pop(); // {10, 5, 9}
+  pop(); // {10, 9}
+  cout << top() << '\n'; // 9
+  push(5); push(15); // {10, 9, 5, 15}
+  cout << top() << '\n'; // 5
+  pop(); // {10, 9, 15}
+  cout << top() << '\n'; // 9
+}
+
+int main(){
+  test();
+}

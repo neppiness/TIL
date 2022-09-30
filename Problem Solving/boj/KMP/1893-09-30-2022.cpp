336571aaa6def462f13576914d0769ba3eb0315d
neppiness <0414kjh@naver.com>
2022-09-30 22:29:08 +0900

    1st commit on September 30th, 2022

--- Problem Solving/boj/KMP/1893-09-30-2022.cpp
+++ Problem Solving/boj/KMP/1893-09-30-2022.cpp
@@ -1,13 +1,36 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-vector<int> f_fun;
+vector<int> f_fun, ans;
 string a, w, s;
+int cnt[64];
+int oa[257], ma[257];
 
 vector<int> failure(){
   vector<int> f(w.size());
-
+  for(int i = 1; i < w.size(); i++){
+    int j = f[i-1];
+    while(j > 0 && w[i] != w[j]) j = f[j-1];
+    if(w[i] == w[j]) f[i] = ++j;
+  }
   return f;
+}
+
+void solve(){
+  int as = a.size();
+  for(int k = 0; k < as; k++){
+    for(int i = 0; i < as; i++) {
+      ma[a[i]] = (as + oa[a[i]] - k)%as;
+      cout << ma[a[i]] << ' ';
+    }
+    cout << endl;
+    int j = 1;
+    for(int i = 0; i < s.size(); i++){
+      while(j > 0 && oa[s[i]] != ma[w[j]]) j = f_fun[j-1];
+      if(oa[s[i]] == ma[w[j]]) j++;
+      if(j == w.size()) cnt[k]++;
+    }
+  }
 }
 
 int main(void){
@@ -16,8 +39,26 @@ int main(void){
 
   int t; cin >> t;
   while(t--){
-    cin >> a >> w >> s;
-    f_fun = failure();
     vector<int> ().swap(f_fun);
+    vector<int> ().swap(ans);
+    fill(cnt, cnt + 64, 0);
+
+    cin >> a >> w >> s;
+    for(int i = 0; i < a.size(); i++) oa[a[i]] = i;
+
+    f_fun = failure();
+    solve();
+
+    for(int k = 3; k <= a.size(); k++)
+      if(cnt[k] == 1) ans.push_back(k);
+    
+    if(ans.size() == 0) cout << "no solution\n";
+    else if(ans.size() == 1)
+      cout << "unique: " << ans[0] << '\n';
+    else {
+      cout << "ambiguous: ";
+      for(int a : ans) cout << a << ' ';
+      cout << '\n';
+    }
   }
 }

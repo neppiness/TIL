850865e9050a10911c7f3222093e8269e4c8d951
neppiness <0414kjh@naver.com>
2022-10-09 21:51:32 +0900

    1st commit on October 9th, 2022

--- Problem Solving/boj/solvedac/11727-10-09-2022.cpp
+++ Problem Solving/boj/solvedac/11727-10-09-2022.cpp
@@ -1,9 +1,18 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int ans[1002];
 int main(void) {
   ios::sync_with_stdio(0);
   cin.tie(0);
 
+  ans[0] = 1;
+  ans[1] = 1;
+  ans[2] = 3;
+
+  int n; cin >> n;
+  for(int i = 3; i <= n; i++)
+    ans[i] = (ans[i-1]*ans[1] + ans[i-2]*(ans[2] - 1)) % 10007;
   
+  cout << ans[n];
 }

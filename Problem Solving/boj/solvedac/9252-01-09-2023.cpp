fdfaf45b1a124c6eef3cc1a9fc4eaf8b5c5a30f1
neppiness <0414kjh@naver.com>
2023-01-15 23:35:40 +0900

    update: TIL on January 15th, 2023

--- Problem Solving/boj/solvedac/9252-01-09-2023.cpp
+++ Problem Solving/boj/solvedac/9252-01-09-2023.cpp
@@ -1,41 +1,26 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-const int MX = 1'000;
+const int MX = 1000;
 
-int cache[MX + 2][MX + 2]; // cache[keyidx][lckidx]
 string key, lck;
+int len[MX + 2][MX + 2]; // len[keyidx][lckidx]
+int idx[MX + 2][MX + 2]; // idx[keyidx][lckidx]
 
-void solve() {
-  for(int keyidx = 0; keyidx < key.length(); keyidx++) {
-    for(int lckidx = 0; lckidx < lck.length(); lckidx++) {
-      bool ismatched = (key[keyidx] == lck[lckidx]);
-      int &c = cache[keyidx + 1][lckidx + 1];
-      c = cache[keyidx][lckidx] + ismatched;
+int solve(int keyidx, int lckidx) {
+  if(keyidx == key.length()) return;
+  int &tmplen = len[keyidx][lckidx];
+  if(tmplen != -1) return tmplen;
+  if(keyidx == 0)
+    return tmplen = (key[keyidx] == lck[lckidx]);
 
-      int &pl = cache[keyidx + 1][lckidx];
-      int &pk = cache[keyidx][lckidx + 1];
-      c = max({c, pk, pl});
-    }
-  }
-}
+  int &tmpidx = idx[keyidx][lckidx];
+  tmpidx = idx[keyidx - 1][lckidx];
 
-void print() {
-  int len = cache[key.length()][lck.length()];
-  cout << len << '\n';
-
-  int x = idx[key.length()][lck.length()];
-  stack<char> st;
-
-  if(x == -1) return;
-  while(len != 0) {
-    st.push(lck[x]);
-    x = idx[key.length()][x];
-    len--;
-  }
-  while(!st.empty()) {
-    cout << st.top();
-    st.pop();
+  int mx = -1;
+  for(int lidx = 0; lidx < lckidx; lidx) {
+    int t = solve(keyidx - 1, lidx);
+    if(mx >= t) 
   }
 }
 
@@ -43,10 +28,10 @@ int main() {
   ios::sync_with_stdio(0);
   cin.tie(0);
 
+  memset(cache, -1, sizeof(cache));
+
   cin >> key >> lck;
-
-  memset(idx, -1, sizeof(idx));
-
-  solve();
-  print();
+  for(int i = 0; i < lck.length(); i++) {
+    
+  }
 }

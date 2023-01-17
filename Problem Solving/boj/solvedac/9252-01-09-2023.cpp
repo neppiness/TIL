8acc1bdf1836d0e9e90b63e4f01cebcd6b4ba9cc
neppiness <0414kjh@naver.com>
2023-01-18 00:52:11 +0900

    update: TIL on January 17th, 2023

--- Problem Solving/boj/solvedac/9252-01-09-2023.cpp
+++ Problem Solving/boj/solvedac/9252-01-09-2023.cpp
@@ -2,25 +2,31 @@
 using namespace std;
 
 const int MX = 1000;
+const int INF = 0x3f3f3f3f;
 
 string key, lck;
 int len[MX + 2][MX + 2]; // len[keyidx][lckidx]
-int idx[MX + 2][MX + 2]; // idx[keyidx][lckidx]
+pair<int, int> idx[MX + 2][MX + 2]; // idx[curkeyidx][curlckidx] = {prvkeyidx, prvlckidx}`
 
-int solve(int keyidx, int lckidx) {
-  if(keyidx == key.length()) return;
+void solve(int keyidx, int lckidx) {
+  if(keyidx > key.length()) return;
   int &tmplen = len[keyidx][lckidx];
-  if(tmplen != -1) return tmplen;
-  if(keyidx == 0)
-    return tmplen = (key[keyidx] == lck[lckidx]);
-
-  int &tmpidx = idx[keyidx][lckidx];
+  auto &tmpidx = idx[keyidx][lckidx];
+  // case i. 이전 keyidx와 동일한 경우: 단순 할당 처리
+  tmplen = len[keyidx - 1][lckidx];
   tmpidx = idx[keyidx - 1][lckidx];
 
-  int mx = -1;
-  for(int lidx = 0; lidx < lckidx; lidx) {
-    int t = solve(keyidx - 1, lidx);
-    if(mx >= t) 
+  // case ii. 이전 lckidx와 동일한 경우: 비교를 통한 값 할당
+  tmplen = max(tmplen, len[keyidx][lckidx - 1]);
+  if(idx[keyidx][lckidx - 1].second < tmpidx.second)
+    tmpidx = idx[keyidx][lckidx - 1];
+
+  // case iii. keyidx - 1과 lckidx에 이번에 매칭된 문자가 더해지는 경우
+  bool ismatched = (key[keyidx] == lck[lckidx]);
+  int l = len[keyidx - 1][lckidx - 1] + ismatched;
+  if(l > tmplen) {
+    tmplen = l;
+    tmpidx = {keyidx - 1, lckidx - 1};
   }
 }
 
@@ -28,10 +34,13 @@ int main() {
   ios::sync_with_stdio(0);
   cin.tie(0);
 
-  memset(cache, -1, sizeof(cache));
-
   cin >> key >> lck;
-  for(int i = 0; i < lck.length(); i++) {
+  for(int i = 1; i <= lck.length(); i++)
+    solve(i, 0);
+
+  cout << len[key.length()][lck.length()] << '\n';
+  auto p = idx[key.length()][lck.length()];
+  while(p != {0, 0}) {
     
   }
 }

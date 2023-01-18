0b665132d98eb3aaeb32eb95b0dd824970a42927
neppiness <0414kjh@naver.com>
2023-01-18 18:52:50 +0900

    update: TIL on January 18th, 2023

--- Problem Solving/boj/Dynamic programming/14003-01-18-2023.cpp
+++ Problem Solving/boj/Dynamic programming/14003-01-18-2023.cpp
@@ -4,22 +4,25 @@ using namespace std;
 const int INF = 0x3f3f3f3f;
 const int MX = 1'000'000;
 
-int lel[MX + 2]; // last element of LIS, indexed by LIS len.
+int ile[MX + 2]; // index of last element, indexed by LIS len.
 int a[MX];
 
 int main() {
   ios::sync_with_stdio(0);
   cin.tie(0);
 
-  fill(lel + 1, lel + MX + 2, INF);
-  lel[0] = -1, lel[1] = 0;
+  fill(ile + 1, ile + MX + 2, INF);
+  ile[0] = 0; a[0] = -INF;
 
   int n; cin >> n;
-  for(int i = 0; i < n; i++)
+  for(int i = 1; i <= n; i++)
     cin >> a[i];
   
-  for(int i = 0; i < n; i++) {
-    int *cur = lower_bound(lel, lel + n + 1, a[i]);
-    
+  for(int i = 1; i <= n; i++) {
+    int len = lower_bound(ile, ile + n + 1, i) - ile;
+    if(a[ile[len - 1]] < a[i]) ile[len] = i;
+    else ile[len - 1] = i;
   }
+  int ans = lower_bound(ile, ile + n + 2, INF) - ile - 1;
+  cout << ans << '\n';
 }

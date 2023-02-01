f9585d1dba2dcd0d1bd67c2348a654f373bfa5d7
neppiness <0414kjh@naver.com>
2023-02-01 23:26:21 +0900

    update: TIL on February 1st, 2023

--- Problem Solving/boj/solvedac/1208-02-01-2023.cpp
+++ Problem Solving/boj/solvedac/1208-02-01-2023.cpp
@@ -1,25 +1,48 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-const int MX = 40;
+int n, s;
+long long ans;
 
-int n, s, ans;
-int a[MX + 2], ps[MX + 2];
+int sz[2];
+vector<int> a[2], sa[2];
+
+void setup(int idx) {
+  vector<bool> comb(sz[idx], 0);
+  for(int i = 0; i <= sz[idx]; i++) {
+    if(i != 0) comb[sz[idx] - i] = 1;
+    do {
+      int sum = 0;
+      for(int j = 0; j < sz[idx]; j++)
+        if(comb[j]) sum += a[idx][j];
+      sa[idx].push_back(sum); 
+    } while(next_permutation(comb.begin(), comb.end()));
+  }
+  sort(sa[idx].begin(), sa[idx].end());
+}
 
 int main() {
   ios::sync_with_stdio(0);
   cin.tie(0);
 
   cin >> n >> s;
-  for(int i = 0; i < n; i++)
-    cin >> a[i];
+  sz[0] = n / 2;
+  sz[1] = n - sz[0];
 
-  for(int i = 1; i <= n; i++)
-    ps[i] += a[i - 1] + ps[i - 1];
-  
-  for(int i = 1; i <= n; i++)
-    for(int j = 0; j < i; j++)
-      if(ps[i] - ps[j] == s) ans++;
+  for(int i = 0; i < 2; i++)
+    for(int j = 0; j < sz[i]; j++) {
+      int x; cin >> x;
+      a[i].push_back(x);
+    }
 
+  for(int i = 0; i < 2; i++)
+    setup(i);
+
+  for(auto x : sa[0]) {
+    auto lb = lower_bound(sa[1].begin(), sa[1].end(), -x);
+    auto ub = upper_bound(sa[1].begin(), sa[1].end(), -x);
+    ans += ub - lb;
+  }
+  if(s == 0) ans--;
   cout << ans;
 }

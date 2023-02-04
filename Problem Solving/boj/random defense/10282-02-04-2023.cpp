5f1497571f8b2e9043771347b0458d774830c97b
neppiness <0414kjh@naver.com>
2023-02-04 14:44:54 +0900

    update: TIL on February 4th, 2023

--- Problem Solving/boj/random defense/10282-02-04-2023.cpp
+++ Problem Solving/boj/random defense/10282-02-04-2023.cpp
@@ -3,33 +3,52 @@ using namespace std;
 
 const int NMX = 10'000;
 const int MMX = 100'000;
+const int INF = 0x7f7f7f7f;
 
 vector<pair<int, int>> adj[NMX + 2];
-int dist[NMX + 2], ind[NMX +2];
+int dist[NMX + 2];
 
-int n, m, c;
+int n, m, st;
 
 void setup() {
-  memset(dist, -1, sizeof(dist));
-  memset(ind, 0, sizeof(ind));
+  memset(dist, 0x7f, sizeof(dist));
 
-  cin >> n >> m >> c;
+  cin >> n >> m >> st;
   for(int i = 1; i <= n; i++)
     adj[i].clear();
 
   while(m--) {
-    int u, v, t;
-    cin >> u >> v >> t;
-    adj[v].push_back({t, u});
-    ind[u]++;
+    int u, v, cost;
+    cin >> u >> v >> cost;
+    adj[v].push_back({cost, u});
   }
 }
 
 void solve() {
   setup();
-  vis[c] = 1;
+  priority_queue< pair<int, int>, 
+      vector<pair<int, int>>,
+      greater<pair<int, int>> > pq;
+  pq.push({0, st});
+  dist[st] = 0;
 
-
+  while(!pq.empty()) {
+    auto [co, u] = pq.top(); pq.pop();
+    if(dist[u] != co) continue;
+    for(auto [dco, v] : adj[u]) {
+      dco += co;
+      if(dist[v] <= dco) continue;
+      dist[v] = dco;
+      pq.push({dco, v});
+    }
+  }
+  int cnt = 0, ans = 0;
+  for(int i = 1; i <= n; i++) {
+    if(dist[i] == INF) continue;
+    cnt++;
+    ans = max(ans, dist[i]);
+  }
+  cout << cnt << ' ' << ans << '\n';
 }
 
 int main() {

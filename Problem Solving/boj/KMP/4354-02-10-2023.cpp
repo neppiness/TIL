bf7aff66ec6aa1f564d10579ea60230db648f8ba
neppiness <0414kjh@naver.com>
2023-02-10 17:47:13 +0900

    update: TIL on February 10th, 2023

--- Problem Solving/boj/KMP/4354-02-10-2023.cpp
+++ Problem Solving/boj/KMP/4354-02-10-2023.cpp
@@ -2,30 +2,24 @@
 using namespace std;
 
 const int MX = 1'000'000 + 2;
+
 int f[MX];
 
 int main() {
   ios::sync_with_stdio(0);
   cin.tie(0);
 
-  string s; getline(cin, s);
+  string s; cin >> s;
   while(s[0] != '.') {
-    int j = 0, cnt = 0;
-    for(int i = 1; i < (int)s.size(); i++) {
+    int j = 0;
+    for(int i = 1; i < s.size(); i++) {
+      j = f[i - 1];
       while(j > 0 && s[i] != s[j]) j = f[j - 1];
       if(s[i] == s[j]) f[i] = ++j;
     }
-    string ss = s.substr(0, s.size() - f[s.size() - 1]);
-    j = 0;
-    for(int i = 0; i < (int)s.size(); i++) {
-      while(j > 0 && s[i] != ss[j]) j = f[j - 1];
-      if(s[i] == ss[j]) j++;
-      if(j == (int)ss.size()) {
-        cnt++;
-        j = f[j - 1];
-      }
-    }    
-    cout << cnt << '\n';
-    getline(cin, s);
+
+
+
+    cin >> s;
   }
 }

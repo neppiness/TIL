28c194fa513f50a972f0e10d9ddffa841b792548
neppiness <0414kjh@naver.com>
2023-02-15 00:17:46 +0900

    update: TIL on February 14th, 2023

--- Problem Solving/boj/Math/20149-02-13-2023.cpp
+++ Problem Solving/boj/Math/20149-02-13-2023.cpp
@@ -5,9 +5,9 @@ struct vec {
   double x, y;
   explicit vec(double x_ = 0, double y_ = 0): x(x_), y(y_) {}
 
-  bool operator < (const vec &rhs) { return (x != rhs.x ? x < rhs.x : y < rhs.y ); }
-  vec operator - (const vec &rhs) { return vec(x - rhs.x, y - rhs.y); }
-  double cross(const vec &rhs) { return (x * rhs.y - y * rhs.x); }
+  bool operator < (const vec &r) { return (x != r.x ? x < r.x : y < r.y ); }
+  vec operator - (const vec &r) { return vec(x - r.x, y - r.y); }
+  double cross(const vec &r) { return (x * r.y - y * r.x); }
 };
 
 int main() {
@@ -16,5 +16,5 @@ int main() {
 
   cout << fixed;
   cout.precision(9);
-  
+  int right;
 }
